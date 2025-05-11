c7cafefa47e3a99711b891d3e61003e38f31c944
jjuuny2008 <62197580+jjuuny2008@users.noreply.github.com>
2025-05-11 12:37:03 +0900

    AAAAA

--- Assets/02.Scripts/FirstScript.cs
+++ Assets/02.Scripts/FirstScript.cs
@@ -6,21 +6,9 @@ using UnityEngine;
 
 
 
-enum state
-{
-    None,
-    WARNING,
-    DANGER,
-    DEAD
-}
 
-enum Fruit
-{
-    Apple,
-    Banana,
-    Peach,
-    Orange
-}
+
+
 
 
 
@@ -29,77 +17,34 @@ public class FirstScript : MonoBehaviour
     // Start is called before the first frame update
 
 
-    float hp = 100.0f;
-    float incomeDamage = 20.0f;
-    state state=state.None;
-    Fruit Fruit = Fruit.Apple;
 
+
+    void EnemyAttack()
+    {
+        Debug.Log("Enemy Attack");
+        Debug.Log("Enemy Damage");
+        Debug.Log("Enemy HP UI Update");
+    }
+
+    int thisyear()
+    {
+        return 2025;
+    }
+
+    float NextYear()
+    {
+        return thisyear() + 1;
+    }
     void Start()
     {
-        Debug.Log("Game Started");
+        Debug.Log(NextYear());
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(hp<=60)
-        {
-            state = state.WARNING;
-        }
-        if (hp <= 40)
-        {
-            state = state.DANGER;
-        }
-        if (hp <= 0)
-        {
-            state = state.DEAD;
-        }
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            hp -= incomeDamage;
-
-            switch (state)
-            {
-                case state.DEAD:
-                    Debug.Log("GameOver");
-                    break;
-                case state.WARNING:
-                    Debug.Log("Emenrgency!!");
-                    break
-            ;   case state.DANGER:
-                    Debug.Log("Watch out");
-                    break;
-                default:
-                    Debug.Log("Current Health is " + hp);
-                    break;
-
-            }
-            
-        }
-
-        if (Input.GetKeyDown(KeyCode.W))
-        {
-            switch (Fruit)
-            {
-                case Fruit.Apple:
-                    Debug.Log("Apple is yammy");
-                    Fruit = Fruit.Banana;
-                    break;
-                case Fruit.Banana:
-                    Debug.Log("Banana is yammy");
-                    Fruit = Fruit.Peach;
-                    break;
-                case Fruit.Peach:
-                    Debug.Log("Peach is yammy");
-                    Fruit = Fruit.Orange;
-                    break;
-                case Fruit.Orange:
-                    Debug.Log("Orange is yammy");
-                    Fruit = Fruit.Apple;
-                    break;
-            }
-        }
+       
 
     }
 }
